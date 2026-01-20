a97292dc15a7611331cdd3938c71f75c74abbc9c
xuchenyang6688 <xuchenyang6688@163.com>
2026-01-20 15:39:07 +0800

    141 Linked List Cycle

---
+++ solutions/linked_list/linkedListCycle.go
@@ -0,0 +1,14 @@
+package linked_list
+
+func hasCycle(head *ListNode) bool {
+	slow := head
+	fast := head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+}
